188f7db3f3ab6d804b126c534c0790f697e7f813
Nenolich <ne0fun907@gmail.com>
2018-04-06 21:13:49 +0300

    elevator in progress!

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+
+    private Rigidbody2D rigBody;
+
+	void Start ()
+    {
+        rigBody = GetComponent<Rigidbody2D>();
+        InvokeRepeating("Elevate",1.5f,3f);
+	}
+	
+	void Elevate()
+    {
+        rigBody.AddForce(new Vector2(0f, 1000f));
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,36 +2,49 @@
 using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
-{	
-	public float health = 100f;
+{
+    public float maxHealth = 100f;
+    public float health = 100f;
+    public float thumbSize = 10f;
 	public AudioClip[] ouchClips;
     public GUIStyle healthSliderStyle;
     public GUIStyle healthThumbStyle;
+    public GUIStyle healthSliderBackgroundStyle;
 
-	private PlayerController playerControl;		
+    private PlayerController playerControl;		
 	private Animator anim;						
     private SpriteRenderer spriteRenderer;
+    private Texture healthSliderBackgroundTexture;
 
-	void Awake ()
+    void Awake ()
 	{
 		playerControl = GetComponent<PlayerController>();
 		anim = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-	}
+        healthSliderBackgroundTexture = new Texture();
+
+    }
 
     private void OnGUI()
     {
-        GUI.HorizontalSlider(new Rect(Screen.width-Screen.width*0.2f, Screen.height*0.05f, Screen.width * 0.15f, Screen.height*0.03f),health,0f,100f, healthSliderStyle, healthThumbStyle);
+        Rect sliderRect=new Rect(Screen.width - Screen.width * 0.2f, Screen.height * 0.05f, Screen.width * 0.15f, Screen.height * 0.03f);
+        GUI.HorizontalSlider(sliderRect, health,0f, maxHealth, healthSliderStyle, healthThumbStyle);
+        if (health < maxHealth)
+        {
+            GUI.Box(new Rect(sliderRect.xMin + sliderRect.width * health / maxHealth + thumbSize * (1-health / maxHealth) + thumbSize, sliderRect.yMin,
+            sliderRect.width - sliderRect.width* health / maxHealth - thumbSize * (1 - health / maxHealth) - thumbSize, sliderRect.height),
+            healthSliderBackgroundTexture, healthSliderBackgroundStyle);
+        }
     }
 
 	public void TakeDamage (int damageAmount)
 	{
-		health -= damageAmount;
+        health = health - damageAmount < 0 ? 0 : health - damageAmount;
 
 		int i = Random.Range (0, ouchClips.Length);
 		AudioSource.PlayClipAtPoint(ouchClips[i], transform.position);
 
-        if (health < 0f)
+        if (health == 0f)
         {
             Collider2D[] cols = GetComponents<Collider2D>();
             foreach (Collider2D c in cols)
